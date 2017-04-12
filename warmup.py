f8e844dcdfadc6b766b4f71b52c1ac1fb7c883f2
krystian77 <k.blajda1@gmail.com>
2017-04-12 08:37:27 +0200

    Add files via upload

--- warmup.py
+++ warmup.py
@@ -1,21 +1,21 @@
-
+import math
 
 def helloworld():
     print("Hello, world!")
-   
+helloworld()    
 
 def board():
     pion = '|'.join(['  ']*3)
     poziom = '\n{}\n'.format('-'*8)
     print(pion,pion,pion,sep=poziom)
-    
+board()    
 def tictactoe():
     pion = 'H'.join(['  |  |  ']*3) + '\n'
     poziom ='H'.join(['--+--+--']*3) + '\n'
     blok = pion.join([poziom]*3)
     separator = '+'.join(['='*8]*3) + '\n'
     print(blok,blok,blok,sep=separator)
-
+tictactoe()
 def multiples():
     for i in range (0,1002):
         if i%5==0:
@@ -23,7 +23,7 @@ def multiples():
         elif i%3==0:
             print(i,end=' ')
         i=i+1
-
+multiples()
 def collatz(number):
     if number%2==0:
         print(number, "->", end=' '),
@@ -36,10 +36,20 @@ def collatz(number):
             print(number, "->", end=' '),
             number=3*number+1            
             collatz(number)
+collatz(55)
+def converter():
+    while True:
+        try:
+            fahr = float(input("Temperature F? "))
+        except KeyboardInterrupt:
+            print("\nExiting converter...")
+            break
+        except ValueError as exc:
+            print(exc)
+        else:
+            cels = (fahr - 32) * 5 / 9
+          
+            print("It is {} degrees Celsius".format(cels))
+converter()            
 
-def ftoc(temp):
-    celsius = (temp - 32) * (5/9)
-    print(celsius)
-    
 
-ftoc(88)
